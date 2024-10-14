42f178f1b18008e20fde42f592df37eafb847d68
Arundurai29 <114252340+Arundurai29@users.noreply.github.com>
2024-10-14 19:13:17 +0530

    first push

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',  // Enables static HTML export
-    trailingSlash: true,  // Ensures trailing slashes for all URLs
-  };
+const nextConfig = {};
   
   module.exports = nextConfig;
   
